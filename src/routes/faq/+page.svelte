617de3f416f30d86c6a92e9818c5e3c2fc147542
marcosantonastasi <marco.santonastasi@gmail.com>
2022-11-25 06:49:00 +0100

    WIP mobile layout FAQs

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import Faq from './Faq.svelte';
-	// import FaqMobile from './FaqMobile.svelte';
+	import FaqMobile from './FaqMobile.svelte';
+
+	let innerWidth: number;
 </script>
 
 <svelte:head>
 	<title>Frequently Asked Questions - Nuvolaris: Join the serverless revolution!</title>
 </svelte:head>
 
-<Faq />
-<!-- <FaqMobile /> -->
+<svelte:window bind:innerWidth />
+{#if innerWidth > 960}
+	<Faq />
+{:else}
+	<FaqMobile />
+{/if}
